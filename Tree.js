efadfce805498ebf6c621db8fddfdde8bc0a47de
Sheishori <sheishori@gmail.com>
2022-12-12 14:45:48 +0100

    Add a buildTree function to the Tree module

--- Tree.js
+++ Tree.js
@@ -1,5 +1,19 @@
-import Node from './Node';
+import Node from './Node.js';
 
 const Tree = (array) => {
-	let root = null;
+	const buildTree = (array, start = 0, end = array.length - 1) => {
+		if (start > end) return null;
+
+		const middle = Math.floor((start + end) / 2);
+		const left = buildTree(array, start, middle - 1);
+		const right = buildTree(array, middle + 1, end);
+
+		return Node(array[middle], left, right);
+	};
+
+	let root = buildTree(array);
+
+	return { root };
 };
+
+export default Tree;
