4bb5c1da45c82c32b46064352b698c60090c0b85
Sheishori <sheishori@gmail.com>
2022-12-12 14:45:14 +0100

    Make the Node module more concise

--- Node.js
+++ Node.js
@@ -1,24 +1,5 @@
-const Node = (root) => {
-	let leftChild = null;
-	let rightChild = null;
-
-	const setLeftChild = (node) => {
-		leftChild = node;
-	};
-
-	const setRightChild = (node) => {
-		rightChild = node;
-	};
-
-	const getLeftChild = () => {
-		return leftChild;
-	};
-
-	const getRightChild = () => {
-		return rightChild;
-	};
-
-	return { root, setLeftChild, setRightChild, getLeftChild, getRightChild };
+const Node = (root, left = null, right = null) => {
+	return { root, left, right };
 };
 
 export default Node;
